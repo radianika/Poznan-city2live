d08f14fc60aec1ebfe7f000a6fd4bc8762fb39f9
radianika <radianika@gmail.com>
2018-11-19 01:02:45 +0200

    map

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,17 @@
 function initMap() {
+
   var map = new google.maps.Map(document.querySelector("#map"), {
     zoom: 14,
-    center: { lat: 52.425813, lng: 16.959263 }
+    center: { lat: 52.425813, lng: 16.959263 },
+		scrollwheel:  false,
+		disableDefaultUI: true
   });
 
   var image = "./images/map-marker.png";
   var marker = new google.maps.Marker({
     position: { lat: 52.427081, lng: 16.937841 },
     map: map,
-    icon: image
+    icon: image,
+		title: 'Poznan'
   });
 }
